a4786bbd923508f64dd06b6b362afeaa210f9d8f
vanerbauza <223787312+vanerbauza@users.noreply.github.com>
2025-12-02 22:16:02 -0600

    landing final + flujo premium + login

--- docs/js/login.js
+++ docs/js/login.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // /docs/js/login.js
 import { auth } from "./firebase-init.js";
 import {
@@ -17,6 +18,29 @@ btn?.addEventListener("click", async () => {
     localStorage.setItem("bauza_email", user.email || "");
     localStorage.setItem("bauza_uid", user.uid);
 
+=======
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3a7cv735RQPYsXMdn4KWQ-NDugL7WyfI",
+  authDomain: "studio-6473341422-75630.firebaseapp.com",
+  projectId: "studio-6473341422-75630",
+  storageBucket: "studio-6473341422-75630.firebasestorage.app",
+  messagingSenderId: "240684953453",
+  appId: "1:240684953453:web:6027f3b025c9ee22e8b464"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+document.getElementById("btn-google")?.addEventListener("click", async () => {
+  try {
+    const { user } = await signInWithPopup(auth, provider);
+    const token = await user.getIdToken();
+    localStorage.setItem("bauza_token", token);
+>>>>>>> ae659de (landing final + flujo premium + login)
     location.href = "./osint.html";
   } catch (e) {
     console.error(e);
