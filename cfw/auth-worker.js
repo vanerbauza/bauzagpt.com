f0a75285ec2d380e07a28eae32853066e709b96d
bauza_phyton <223787312+vanerbauza@users.noreply.github.com>
2025-12-25 03:02:25 -0600

    Update auth-worker.js

--- cfw/auth-worker.js
+++ cfw/auth-worker.js
@@ -3,7 +3,5 @@ export default {
     const url = new URL(request.url);
     if (url.pathname !== "/verify-google") {
       return new Response("Not Found", {status:404});
-    }
-    // ... (pega AQUÍ el cuerpo del worker anterior, reemplazando este return)
   }
 };
