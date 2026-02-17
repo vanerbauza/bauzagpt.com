b064d487a4175adabd746d08b383a0138c72dacf
vanerbauza <vanerbauza@users.noreply.github.com>
2026-02-16 21:06:54 -0600

    Move generated frontend into docs/ for GitHub Pages

--- frontend/js/auth.js
+++
@@ -1,5 +0,0 @@
-export async function getToken() {
-  const user = firebase.auth().currentUser;
-  if (!user) throw new Error('not_authenticated');
-  return await user.getIdToken();
-}
--- frontend/js/download.js
+++
@@ -1,9 +0,0 @@
-const API = 'https://bauzagpt-backend.fly.dev';
-
-const params = new URLSearchParams(window.location.search);
-const orderId = params.get('orderId');
-const token = sessionStorage.getItem('token');
-
-document.getElementById('downloadBtn').onclick = () => {
-  window.location.href = API + '/api/orders/' + orderId + '/pdf?token=' + token;
-};
