fc868c7829d4a309c894e346a8be110f35c01149
vanerbauza <bauza723@gmail.com>
2026-03-05 17:36:31 -0600

    replaced docs js firebace-init and others docs

--- docs/js/firebase-init.js
+++ docs/js/firebase-init.js
@@ -1,19 +1,20 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBaHoDEcL8ZjVoJJiTbcSZGAiKJJgfmfZc",
-    authDomain: "bauzagpt-fbdca.firebaseapp.com",
-    projectId: "bauzagpt-fbdca",
-    storageBucket: "bauzagpt-fbdca.firebasestorage.app",
-    messagingSenderId: "766060384370",
-    appId: "1:766060384370:web:fc19c978fe43be4e86b2eb"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBaHoDEcL8ZjVoJJiTbcSZGAiKJJgfmfZc",
+  authDomain: "bauzagpt-fbdca.firebaseapp.com",
+  projectId: "bauzagpt-fbdca",
+  storageBucket: "bauzagpt-fbdca.firebasestorage.app",
+  messagingSenderId: "766060384370",
+  appId: "1:766060384370:web:fc19c978fe43be4e86b2eb"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
