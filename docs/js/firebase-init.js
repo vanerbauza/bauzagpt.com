e135c374f9626c2159f8ad72fddf77fd21dc2ee9
vanerbauza <bauza723@gmail.com>
2026-03-05 14:44:18 -0600

    fix autentication firebase-init.js

--- docs/js/firebase-init.js
+++ docs/js/firebase-init.js
@@ -1,26 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { 
-  initializeAuth,
-  browserLocalPersistence,
-  browserPopupRedirectResolver,
-  GoogleAuthProvider
-} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBaHoDEcL8ZjVoJJiTbcSZGAiKJJgfmfZc",
-  authDomain: "bauzagpt.com",
-  projectId: "bauzagpt-fbdca",
-  storageBucket: "bauzagpt-fbdca.firebasestorage.app",
-  messagingSenderId: "766060384370",
-  appId: "1:766060384370:web:fc19c978fe43be4e86b2eb"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBaHoDEcL8ZjVoJJiTbcSZGAiKJJgfmfZc",
+    authDomain: "bauzagpt-fbdca.firebaseapp.com",
+    projectId: "bauzagpt-fbdca",
+    storageBucket: "bauzagpt-fbdca.firebasestorage.app",
+    messagingSenderId: "766060384370",
+    appId: "1:766060384370:web:fc19c978fe43be4e86b2eb"
+  };
 
-const app = initializeApp(firebaseConfig);
-
-// Resolver correcto para popups SIN Firebase Hosting
-export const auth = initializeAuth(app, {
-  persistence: browserLocalPersistence,
-  popupRedirectResolver: browserPopupRedirectResolver
-});
-
-export const provider = new GoogleAuthProvider();
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
