a3377f3bd2be70eb1268be01217401cffdc0fa57
vanerbauza <bauza723@gmail.com>
2026-03-16 16:36:21 -0600

    add firebaseconfig agen

--- docs/js/firebase-init.js
+++ docs/js/firebase-init.js
@@ -1,9 +1,8 @@
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,3 +16,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const facebookProvider = new FacebookAuthProvider();
